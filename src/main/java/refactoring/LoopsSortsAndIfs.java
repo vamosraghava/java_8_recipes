003a7306c590744ec3e3375c008ad258273487d9
Ken Kousen <ken.kousen@kousenit.com>
2018-09-19 00:29:43 -0400

    Add class to refactor

--- src/main/java/refactoring/LoopsSortsAndIfs.java
+++ src/main/java/refactoring/LoopsSortsAndIfs.java
@@ -5,9 +5,24 @@ import java.util.stream.Collectors;
 
 public class LoopsSortsAndIfs {
     public static void main(String[] args) {
-        Arrays.stream("this is an array of strings".split(" "))
-                .filter(s -> s.length() % 2 == 0)
-                .sorted(Comparator.comparingInt(String::length))
-                .forEach(System.out::println);
+        String[] strings = "this is an array of strings".split(" ");
+
+        List<String> evenLengths = new ArrayList<>();
+        for (String s : strings) {
+            if (s.length() % 2 == 0) {
+                evenLengths.add(s);
+            }
+        }
+
+        Collections.sort(evenLengths, new Comparator<String>() {
+            @Override
+            public int compare(String s1, String s2) {
+                return s1.length() - s2.length();
+            }
+        });
+
+        for (String s : evenLengths) {
+            System.out.println(s);
+        }
     }
 }
